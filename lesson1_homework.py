39e7b1a021b1911a795ef9cf062dbd562fbc78ae
mikhail_srago <mihanik999@yandex.ru>
2025-07-21 21:24:43 +0300

    Lesson1 and homework finished

---
+++ lesson1_homework.py
@@ -0,0 +1,6 @@
+print("Your name:", input("Enter your name →"))
+print("Your account:", input("Enter your account number →"))
+print("Thanks,", input("Enter amount of money →"), "NIS from your account was send to the most beautiful panda Ling Ling")
+
+print(f"the result of 7 - 5 is {7 - 5}")
+print (f"the result 4.37 * 8.123 = {4.37 * 8.123:.2f}")
---
+++ lesson1_output_input.py
@@ -0,0 +1,20 @@
+# comment 1
+"""
+comment
+multiline
+"""
+
+print("Hello, world!")
+print('Hello, "world"!')
+print("\tTabulation")  # \t create tabulation
+print("repeat " * 5)  # * N create N copies
+print("Hello", end=" ")  # end redefine final symbol of string (instead of NEL)
+print("course", end=" ")
+print("BP-116")
+print("bye \nschool") # \n create NEL
+
+# print("Your name", input("Enter your name →")) # input takes data from user and concatenate with string from print
+
+print(f"5 + 6 = {5 + 6}") # f formats string for other types
+print(f"{5 / 7:.3f}") # :.Nf defines number of decimals (N)
+
