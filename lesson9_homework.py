eb6ee906ff551d7b88f8178888a2e69460f00309
mikhail_srago <mihanik999@yandex.ru>
2025-08-19 19:11:00 +0300

    troubles with zeros solved

--- lesson9_homework.py
+++ lesson9_homework.py
@@ -1,4 +1,4 @@
-# # part 1
+# part 1
 def has_digit_count(number: int, digit: int):
     if digit < 0 or digit > 9:
         return "ERROR!"
@@ -20,26 +20,45 @@ print(has_digit_count(123456759, -6))
 def print_reverse_number(number: int):
     abs_number = abs(number)
     reversed_number = 0
+    zeros_in_end = 0
+    while (abs_number % 10 == 0):
+        abs_number //= 10
+        zeros_in_end += 1        
     while abs_number > 0:
         reversed_number = reversed_number * 10 + abs_number % 10
         abs_number //= 10
     if number < 0:
-        return -reversed_number
-    return reversed_number
-print(print_reverse_number(12345))
-print(print_reverse_number(-67890)) # how to solve trouble with disappearing zeros without using str?
-print(print_reverse_number(1000))
+        print("-", end="")
+        print("0" * zeros_in_end, end="")        
+        print(reversed_number)
+    else:
+        print("0" * zeros_in_end, end="")
+        print(reversed_number)
+print_reverse_number(12345)
+print_reverse_number(-67890) 
+print_reverse_number(1000)
 
 # part 3
 def print_number_in_column(number: int):
     if number < 0:
         print("Number cannot be negative")
         return
-    reversed_number = print_reverse_number(number) # reusing the previous function
+    zeros_in_end = 0
+    while (number % 10 == 0):
+        number //= 10
+        zeros_in_end += 1
+    reversed_number = 0
+    while number > 0:
+        reversed_number = reversed_number * 10 + number % 10
+        number //= 10
     if reversed_number == 0:
         print(0)
         return
-    while reversed_number > 0:
+    while zeros_in_end > 0:
+            print("0")
+            zeros_in_end -= 1
+    while reversed_number > 0:            
         print(reversed_number % 10)
         reversed_number //= 10
 print_number_in_column(12345)
+print_number_in_column(12300)
